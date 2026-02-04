fcfd09c2a13c6d1d239ddd4740ea0ff78df375dc
LuisNava147 <luis.navas147@gmail.com>
2026-02-03 21:01:30 -0600

    Fix: database.py, configuración para aceptar postgreSQL para produccion

--- backend/database.py
+++ backend/database.py
@@ -1,19 +1,23 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE = "sqlite:///./creditsim.db"
+#se busca la URL de la nube. Sino existe se usa SQLite
+SQLALCHEMY_DATABASE = os.getenv("DATABASE_URL","sqlite:///./creditsim.db")
 
-engine = create_engine(
+#Correción para render
+if SQLALCHEMY_DATABASE.startswith("postgres://"):
+    SQLALCHEMY_DATABASE = SQLALCHEMY_DATABASE.replace("postgres://", "postgresql://", 1)
+
+if "sqlite" in SQLALCHEMY_DATABASE:
+    engine = create_engine(
     SQLALCHEMY_DATABASE, connect_args={"check_same_thread":False}
 )
+else:
+    #configuración para postgreSQL
+    engine = create_engine(SQLALCHEMY_DATABASE)
+
 SessionLocal = sessionmaker(autocommit= False, autoflush= False, bind= engine)
 
 Base = declarative_base()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
